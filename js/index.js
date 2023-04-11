773278e78187a373cc49d45c922711bc82e044a6
ragimovalik <ragimovalik@gmail.com>
2023-04-11 16:00:31 +0300

    Some markup and styles added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-console.log('Index js added');
+// Refs
+const rootEl = document.getElementById('root');
+const formBoxEl = document.getElementById('form-box');
+const userForm = document.forms['user-data'];
+const isValidNameEl = document.getElementById('isValid-name');
+const holeEl = document.getElementById('hole');
+const targetEl = document.getElementById('target');
+const startBtnEl = document.getElementById('start-btn');
+
+// Variables
+let score = 0;
+let level = 1;
+
+function submitHandler(event) {
+  event.preventDefault();
+
+  console.log('submit');
+}
+
+function start() {
+  console.log('start');
+}
