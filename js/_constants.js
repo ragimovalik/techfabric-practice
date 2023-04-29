dfafe4a536a953d7261ed81e98e67f9bcce497dd
ragimovalik <ragimovalik@gmail.com>
2023-04-29 11:51:15 +0300

    Styles updated

--- js/_constants.js
+++
@@ -1,7 +0,0 @@
-export const constants = {
-  LEVELUP_COUNT: 10,
-  WIN_SCORE: 50, // counts
-  JUMP_DELAY: 500, // milliseconds
-  JUMP_DURATION: 850, // milliseconds
-  LEVELUP_SPEEDUP_STEP: 50, // milliseconds
-};
--- js/_utils.js
+++
@@ -1,5 +0,0 @@
-// Utils
-export function delay(func, ms) {
-  if (!ms) ms = 500;
-  setTimeout(() => func(), ms);
-}
